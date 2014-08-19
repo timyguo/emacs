65e2dfb1e73cc55d039794ae31a4513f53195e7b
Tim Guo <tim.y.guo@gmail.com>
2014-08-20 08:36:49 +1000

    Update

--- dotemacs.el
+++ dotemacs.el
@@ -119,6 +119,14 @@
 
 (add-to-list 'load-path "~/emacs/packages/haskell-mode")
 (require 'haskell-mode)
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
 
 (add-to-list 'load-path "~/emacs/packages/structured-haskell-mode/elisp")
 (require 'shm)
