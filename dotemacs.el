128baac2eefb95f8f86035ef8c2d7f454ea0d015
Tim Guo <tim.y.guo@gmail.com>
2014-10-26 22:36:23 +1100

    Update

--- dotemacs.el
+++ dotemacs.el
@@ -184,6 +184,27 @@
 (show-paren-mode 1)
 )
 
+(use-package python-mode)
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Windows"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p nil)
+(setq py-switch-buffers-on-execute-p nil)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
+
+(use-package fill-column-indicator)
+(define-globalized-minor-mode
+ global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode t)
+
 (use-package smex)
 
 (use-package scala-mode2)
